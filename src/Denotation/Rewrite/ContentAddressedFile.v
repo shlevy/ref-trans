bdcbd492050b057e3fad13bdfdc3a85df269d1a6
Shea Levy <shea@shealevy.com>
2017-12-16 11:51:24 -0500

    Rename FixedOutput to ContentAddressedFile.

--- src/Denotation/Rewrite/ContentAddressedFile.v
+++ src/Denotation/Rewrite/ContentAddressedFile.v
@@ -8,27 +8,28 @@ Set Universe Polymorphism.
 Set Polymorphic Inductive Cumulativity.
 (* end hide *)
 
-(** * The denotation of a fixed-output file store.
+(** * The denotation of a content-addressed file store.
 
     Files are realised verbatim into the store and identified by
     their hash.
   *)
-Class fixed_output
+Class content_addressed
   {type} {term} `(f : has_files type term)
   {hash_len : nat} (hash : list byte → Vector.t byte hash_len) :=
   { (** Arbitrary file contents can be added to the store. *)
-    add_fixed : list byte → term file;
-    (** The contents of a fixed file added to the store are just the
+    add_file : list byte → term file;
+    (** The contents of a file added to the store are just the
         original contents.
       *)
-    add_fixed_copies : ∀ {bs}, realise (add_fixed bs) = bs;
-    (** Fixed files added to the store can be identified with an
+    add_file_copies : ∀ {bs}, realise (add_file bs) = bs;
+    (** Files added to the store can be identified with an
         identifier that is a function only of the file contents' hash
         and the store in question.
       *)
-    add_fixed_identifiable : ∀ {bs}, identifiable (add_fixed bs);
-    fixed_output_identifier : Vector.t byte hash_len → identifier file;
-    add_fixed_identify_hashes : ∀ {bs},
-      projT1 (identify (add_fixed bs) add_fixed_identifiable) =
-      fixed_output_identifier (hash bs);
+    add_file_identifiable : ∀ {bs}, identifiable (add_file bs);
+    content_addressed_identifier : Vector.t byte hash_len
+                                 → identifier file;
+    add_file_identify_hashes : ∀ {bs},
+      projT1 (identify (add_file bs) add_file_identifiable) =
+      content_addressed_identifier (hash bs);
   }.
